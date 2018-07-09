4f12df9c98af6c0f888aa242e2531710c16a82b0
Nexor <mvrozanti@hotmail.com>
2018-07-09 13:47:51 -0300

    fixed running out of project directory

--- rastrear.sh
+++ rastrear.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 codigo_de_rastreamento="$@"
+call_dir="`pwd`"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+script_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "$script_dir"
 cat correios.js | sed -e 's/ID_OBJETO/'$codigo_de_rastreamento'/g' > correios2.js
 content="`phantomjs correios2.js`"
 ./correios.py "$content"
 rm correios2.js
+cd "$call_dir"
