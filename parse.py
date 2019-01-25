a5bdbd647d9b2988caec214c2a8edcfd31c9b75c
Marcelo <mvrozanti@hotmail.com>
2019-01-25 17:17:44 -0200

    added alternate flow for when obj is not identified

--- parse.py
+++ parse.py
@@ -13,10 +13,18 @@ newest_status = bs.find(lambda tag: tag.name == 'div' \
         and tag.has_attr('class') \
         and tag.get('class')[0] == 'highlightSRO')
 
-history = bs.find(lambda tag: tag.name == 'table').find_all(lambda tag: tag.name == 'tr')
-
-if None not in [newest_status,history]: 
+try:
+    history = bs.find(lambda tag: tag.name == 'table').find_all(lambda tag: tag.name == 'tr')
+except Exception as e:
+    obj_not_found = False
+    for s in bs.strings:
+        if 'o possui dados sobre o objeto info' in s:
+            print('O sistema dos correios não possui dados sobre o objeto informado. ')
+            obj_not_found = True
+            sys.exit(1)
+    if not obj_not_found: print(e)
+if None not in [newest_status,history]:
     print(newest_status.text.replace('\n\n\n','\n').replace('\n\n','\n'))
-    [print(h.text.replace('\n\n\n','\n').replace('\n\n','\n')) for h in history] # 
+    [print(h.text.replace('\n\n\n','\n').replace('\n\n','\n')) for h in history] #
 else: print('Site dos correios mudou, favor reportar este problema.')
 
